4645a2961afef0f3fb4cb051530fa5917fdb3f24
notSagyo <notsagyo@gmail.com>
2023-09-13 17:26:04 -0300

    Comentarios en métodos de array

--- arrays.js
+++ arrays.js
@@ -1,44 +1,44 @@
-// MODIFICAN ================================================================ //
+// MODIFICAN =================================================================//
 // Agrega elemento al final
-// push
+[1, 2, 3].push(4); //=> [1, 2, 3, 4]
 // Elimina ultimo elemento
-// pop
+[1, 2, 3].pop(); //=> [1, 2]
 // Elimina primer elemento
-// shift
+[1, 2, 3].shift(); //=> [2, 3]
 // Agrega elemento al principio
-// unshift
+[1, 2, 3].unshift(0); //=> [0, 1, 2, 3]
 // Invierte el array
-// reverse
+[1, 2, 3].reverse(); //=> [3, 2, 1]
 // Ordena el array
-// sort
+[1, 3, 2].sort(); //=> [1, 2, 3]
 // Quita desde elemento X, Y cantidad
-// splice
+[1, 2, 3].splice(0, 2); //=> [3]
 
-// NO MODIFICAN ============================================================= //
+// NO MODIFICAN ==============================================================//
 // Une un array con otro
-// concat
+[1, 2, 3].concat([4, 5, 6]); //=> [1, 2, 3, 4, 5, 6]
 // Une todos los elementos de un array y devuelve una string
-// join
+[1, 2, 3].join(' - '); // "1 - 2 -3"
 
-// ITERACION ================================================================ //
+// ITERACION =================================================================//
 // DEVUELVE UN ARRAY =================//
 // Devuelve un nuevo array de la misma longitud
-// map
+[1, 2, 3].map((el) => el * 2); //=> [2, 4, 6]
 // Devuelve un nuevo array que cumpla con ciertas condiciones
-// filter
+[1, 2, 3].filter((el) => el > 1); //=> [2, 3]
 // Itera un array, y nos devuelve un único valor
-// reduce
+[1, 2, 3].reduce((a, el) => a + el, 0); //=> 6
 
 // DEVUELVE UN UNICO VALOR ===========//
-// Devuelve un elemento que cumpla con la condicion
-// find
+// Devuelve el primer elemento que cumpla con la condicion
+[1, 2, 3].find((el) => el > 1); //=> 2
 // Devuelve true si algún elemento cumple con la condicion
-// some
-// Devuelve true si TODOS los elementos cumple con la condicion
-// every
+[1, 2, 3].some((el) => el == 2); //=> true
+// Devuelve true si TODOS los elementos cumplen con la condicion
+[1, 2, 3].every((el) => el > 2); //=> false;
 // Devuelve true si el array incluye el elemento que le pasamos
-// includes
+[1, 2, 3].includes(2); //=> true
 
-// NO DEVUELVE NADA ================= //
+// NO DEVUELVE NADA ==================//
 // Itera un array
-// forEach
+[1, 2, 3].forEach((el) => console.log(el)); //=> undefined
